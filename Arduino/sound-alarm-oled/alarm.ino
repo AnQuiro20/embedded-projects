88f1dae7786901590aa9fb94399c53ff2dc93a00
AnQuiro20 <133550403+AnQuiro20@users.noreply.github.com>
2026-03-16 16:29:49 -0600

    Update alarm.ino

--- Arduino/sound-alarm-oled/alarm.ino
+++ Arduino/sound-alarm-oled/alarm.ino
@@ -1,1 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int soundSensor = 7;
+int buzzer = 6;
+int led = 5;
+
+void setup() {
+  pinMode(soundSensor, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("OLED no encontrada");
+    while (true);
+  }
+
+  display.clearDisplay();
+  display.display();
+}
+
+void loop() {
+  int sound = digitalRead(soundSensor);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+
+  if (sound == LOW) {
+    display.setCursor(15, 20);
+    display.println("ALERTA!");
+
+    digitalWrite(led, HIGH);
+    tone(buzzer, 1200);
+  } else {
+    display.setCursor(10, 20);
+    display.println("Silencio");
+
+    digitalWrite(led, LOW);
+    noTone(buzzer);
+  }
+
+  display.display();
+
+  delay(200);
+}
